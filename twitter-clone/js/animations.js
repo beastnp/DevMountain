d3eed4241b894051531a40074b6cbe7933be08a7
Nick Peterson <peterson.nick2@gmail.com>
2015-10-23 15:49:41 -0600

    second

---
+++ twitter-clone/js/animations.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    $('#tweet-controls').hide();
+
+    $('.tweet-compose').focus(function () {
+        $('.tweet-compose').css('height', '5em');
+        $('#tweet-controls').show();
+
+        var charCount = parseInt($('#char-count').text());
+        //        console.log(charCount);
+
+        $('.tweet-compose').keydown(function(key) {
+            if (key.keyCode !== 8) {
+                charCount -= 1;
+                $('#char-count').text(charCount.toString());
+            } else if (charCount < 140 && key.keyCode === 8) {
+                charCount += 1;
+                $('#char-count').text(charCount.toString());
+            }
+            
+            if (charCount <= 10) {
+                $('#char-count').css('color', 'red');
+            } else {
+                $('#char-count').css('color', '#999');
+            }
+            
+            if (charCount < 0) {
+                $('#tweet-submit').hide();
+            } else {
+                $('#tweet-submit').show();
+            }
+
+        })
+
+    });
+
+
+})
