d8c0466cb95b2afae2abce5a5ca2c73f68b1c304
Nick Peterson <peterson.nick2@gmail.com>
2015-10-30 16:23:36 -0600

    second

--- angular-practice/js/mainCtrl.js
+++ angular-practice/js/mainCtrl.js
@@ -1,11 +1,24 @@
 angular.module('myApp').controller('mainCtrl', function($scope, myService) {
     
-    $scope.getData = function() {
-        myService.goGetEm().then(function(swData) {
+    $scope.currentPage = 1;
+    $scope.maxPages = 9;
+    
+    $scope.getData = function(pageNum) {
+        myService.goGetEm(pageNum).then(function(swData) {
             $scope.characterData = swData;
             console.log($scope.characterData);
         })
     };
+    
+    $scope.previous = function() {
+        $scope.currentPage--;
+        $scope.getData($scope.currentPage);
+    }
+    
+    $scope.next = function() {
+        $scope.currentPage++;
+        $scope.getData($scope.currentPage);
+    }
     
     $scope.getData();
 
--- angular-practice/js/myService.js
+++ angular-practice/js/myService.js
@@ -1,12 +1,12 @@
 angular.module('myApp').service('myService', function ($q, $http) {
 
-    var baseUrl = 'http://swapi.co/api/people/'
+    var baseUrl = 'http://swapi.co/api/people/?page='
 
-    this.goGetEm = function() {
+    this.goGetEm = function(pageNum) {
         
         var deferred = $q.defer();
         
-        $http.get(baseUrl).then(function(response) {
+        $http.get(baseUrl + pageNum).then(function(response) {
             var parsedResponse = response.data.results;
             deferred.resolve(parsedResponse);
         })
