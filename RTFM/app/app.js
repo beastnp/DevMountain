b6ece2d8ab025223d4d4085dd0ab9ed2bd0ac6b0
Nick Peterson <peterson.nick2@gmail.com>
2015-11-05 16:21:32 -0700

    final

---
+++ RTFM/app/app.js
@@ -0,0 +1,38 @@
+var app = angular.module('rtfmApp', ['ui.router', 'firebase']);
+
+app.constant('fb', {
+    url: 'https://nick-rtfm-project.firebaseio.com/'
+});
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state('home', {
+            url: '/threads',
+            templateUrl: './app/threads/threads.html',
+            controller: 'threadsCtrl',
+            resolve: {
+                threadsRef: function (threadsService) {
+                    console.log(threadsService.getThreads());
+                    return threadsService.getThreads();
+                }
+            }
+        })
+        .state('thread', {
+            url: '/threads/:threadId',
+            templateUrl: 'app/thread/thread.html',
+            controller: 'threadCtrl',
+            resolve: {
+                threadRef: function (threadsService, $stateParams) {
+                    return threadsService.getThread($stateParams.threadId);
+                },
+                commentsRef: function(threadsService, $stateParams) {
+                    return threadsService.getComments($stateParams.threadId);
+                }
+            }
+        });
+
+    $urlRouterProvider
+        .otherwise('/threads');
+
+});
