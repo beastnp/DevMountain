44dd351da18a86dcc9df2519139c80c199251dde
Nick Peterson <peterson.nick2@gmail.com>
2015-11-10 13:30:01 -0700

    Nov10first

--- no-server-project/app/mainDirective.js
+++ no-server-project/app/mainDirective.js
@@ -4,7 +4,7 @@ angular.module('noServerApp')
 
     return {
         restrict: 'E',
-        template: '<div class="nav-top"><ul><li><a href="/#/home">Home</a></li><li><a href="/#/portfolio">Portfolio</a></li><li><a href="/#/capabilities">Capabilities</a></li><li><a href="/#/contact">Contact</a></li></ul></div>'
+        template: '<div class="nav-top"><img src="nicks-logo2.png" height="75" width="125"></img><ul><li><a href="/#/home">Home</a></li><li><a href="/#/portfolio">Portfolio</a></li><li><a href="/#/capabilities">Capabilities</a></li><li><a href="/#/contact">Contact</a></li></ul></div>'
     };
 
 });
